6f2b681e6e12b2be62ee7571a5f869c31d084a92
Cotton Hou <himcotton@gmail.com>
2025-10-16 05:16:59 +0800

    test for reading both args and stdin

--- spec/main.test.ts
+++ spec/main.test.ts
@@ -11,6 +11,34 @@ import { USAGE } from '#src/help.ts';
 
 describe('main', function () {
 
+    const mnemonic = `
+
+        denial magic satoshi blast forest mixed coffee
+        genuine donkey moon sail cave eyebrow burst load
+
+    `.trim().split(/\s+/);
+
+    it('support reading from both args and stdin', async function () {
+
+        const [ args, stdin ] = await Promise.all([
+
+            new Promise<string>(async function (resolve) {
+                await main(parse(mnemonic), resolve);
+            }),
+
+            new Promise<string>(async function (resolve) {
+                await main(parse([]), resolve, () => mnemonic.join(' '));
+            }),
+
+        ]);
+
+        const address = 'bc1qetljrh848kv8vyqwsvrptju4ghdxn6w367amma';
+
+        ast.assertStrictEquals(args, address);
+        ast.assertStrictEquals(stdin, address);
+
+    });
+
     {
 
         const args = [
