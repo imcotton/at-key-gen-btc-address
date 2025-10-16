7c242a1da643e0430f747bc801f6aff405d2601b
Cotton Hou <himcotton@gmail.com>
2025-10-16 20:05:50 +0800

    test for invalid -a

--- spec/parse.test.ts
+++ spec/parse.test.ts
@@ -9,6 +9,13 @@ import { parse } from '#src/parse.ts';
 
 describe('parse', function () {
 
+    it('throws on invalid account', function () {
+
+        ast.assertThrows(() => parse([        '-a', 'wat' ]));
+        ast.assertThrows(() => parse([ '--account', '4.2' ]));
+
+    });
+
     it('throws on unknown options', function () {
 
         ast.assertThrows(() => parse([ '--wat' ]));
